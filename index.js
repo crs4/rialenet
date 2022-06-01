f52d5c2872129840fba599dec8670508b396227a
Stefano Leone Monni <stefano.monni@crs4.it>
2022-06-01 21:11:13 +0200

    refactoring

--- index.js
+++ index.js
@@ -18,12 +18,8 @@ const PORT = process.env.PORT || 3333
 
 // WENET CONFIG DATA 
 const CLIENT_SECRET = wenet_config.CLIENT_SECRET
-const APP_ID = wenet_config.APP_ID
 const CLIENT_ID = wenet_config.CLIENT_ID
-
 const WENET_URL = wenet_config.WENET_URL
-const COMMUNITY_ID = wenet_config.COMMUNITY_ID
-const TASK_TYPE_ID =  wenet_config.TASK_TYPE_ID //da connettore conversazionale/App logic
 
 
 // ------------------ //
@@ -49,14 +45,12 @@ app.use(session({
   cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 } // 1 week
 }));
 
-
 app.get('/db', async (req, res) => {
   const data = await dbConnector.getAllUsersByRole(dbConnector.Role.student);
   res.send(data);
 });
 
 app.get('/connect', async (req, res) => {
-  
   const passcode = req.query.passcode;
   console.log("connect-to_wenet request (per ottenere authcode (ed external_id)) da passcode:",passcode);
   const user = await dbConnector.getUserByPasscode(passcode)
@@ -81,17 +75,16 @@ app.get('/callback', async (req, res) => {
    */
   const code = req.query.code
   const passcode = req.session.passcode
-
   //const externalId = req.query.external_id
   console.log("SERVER OAUTH CODE:", code)
   // request token
-  const tokens = await requestToken(code);
+  const tokens = await wenetConnector.requestToken(code);
   req.session.tokens = tokens;
   console.log("ACCESS TOKEN RICAVATO!");
   //res.redirect(`${WENET_URL}/prod/hub/frontend/oauth/complete?app_id=${APP_ID}`)
- 
   return res.redirect(`/forum?passcode=${passcode}`);
 })
+
 
 app.get('/tasks', async (req, res) => {
   const passcode = req.query.passcode;
@@ -113,84 +106,9 @@ app.get('/tasks', async (req, res) => {
 
 app.post('/newtask', async (req, res) => {
   console.log("Request body content->>", req.body["content"])
-  const result = await createNewTask(req.session.tokens,req.session.external_id,req.body["content"])
+  const result = await wenetConnector.createNewTask(req.session.tokens,req.session.external_id,req.body["content"])
   res.send(`result di newTask:${result}`)
 })
-
-const createTaskBody = (external_id,content) => {
-  return (
-    {
-      "taskTypeId": TASK_TYPE_ID, //  "626a6745925841535833b638",
-      "appId": APP_ID, //"GOJt2zQA6B",
-      "communityId": COMMUNITY_ID,    //"624594390e5af47df0eed5ea",
-      "requesterId": external_id, //"528",
-      "goal": {
-        "name": "Help 4 for timeline master",
-        "description": `${content}`,
-        "keywords": [
-          "social interaction",
-          "lab"
-        ]
-      }
-    }
-  )
-}
-
-
-const createNewTask = async (tokens,external_id,content) => {
-  const url = `${WENET_URL}/prod/api/service/task`
-  //console.log("requestTokenDetails: Tokens:", tokens);
-  //console.log("requestTokenDetails: Access Token:", tokens.access_token)
-  try {
-    const response = await
-      fetch(url, {
-        headers: {
-          "Authorization": `bearer ${tokens.access_token}`,
-          "Content-Type": "application/json"
-        },
-        method: "POST",
-        body: JSON.stringify(createTaskBody(external_id,content))
-      })
-    const details = await response.json()
-    console.log("CREATE TASK RESPONSE:", details)
-    return details
-  } catch (e) {
-    console.log("error from server in createTask:", e)
-    return `Error:${e}`
-  }
-}
-
-const requestToken = async (oauthCode) => {
-  try {
-    const response = await
-      fetch(`${WENET_URL}/prod/api/oauth2/token`, {
-        body: `grant_type=authorization_code&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${oauthCode}`,
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        method: "POST"
-      })
-    console.log("Response from sever:", response)
-    const content = await response.json();
-    console.log("Json content from response:", content)
-    return content;
-    /*
-    {
-      refresh_token: '8It8uoIVspPOlUUmC6Efp3L1N48Rjy9q',
-      token_type: 'bearer',
-      access_token: 'JBAsvsz1gBa9opce0jOcbN2kvxQBS45v',
-      expires_in: 7200
-    }
-    */
-  } catch (e) {
-    console.log("error from server:", e)
-    return `Error:${e}`
-  }
-
-}
-
-
-
 
 
 app.get('/*', function (req, res) {
--- wenet/wenet_api.js
+++ wenet/wenet_api.js
@@ -4,6 +4,9 @@ const fetch = require("node-fetch");
 
 const WENET_URL = wenet_config.WENET_URL
 const APP_ID = wenet_config.APP_ID
+const COMMUNITY_ID = wenet_config.COMMUNITY_ID
+const TASK_TYPE_ID =  wenet_config.TASK_TYPE_ID //da connettore conversazionale/App logic
+
 
 const getAllTasks = async (tokens) => {
     console.log(`Richiamo getAllTasks() con accessToken:${tokens.access_token}`);
@@ -24,8 +27,82 @@ const getAllTasks = async (tokens) => {
       console.log("error from server in getTasks:", e)
       return `Error:${e}`
     }
+  }
+
+  const createNewTask = async (tokens,external_id,content) => {
+    const url = `${WENET_URL}/prod/api/service/task`
+    //console.log("requestTokenDetails: Tokens:", tokens);
+    //console.log("requestTokenDetails: Access Token:", tokens.access_token)
+    try {
+      const response = await
+        fetch(url, {
+          headers: {
+            "Authorization": `bearer ${tokens.access_token}`,
+            "Content-Type": "application/json"
+          },
+          method: "POST",
+          body: JSON.stringify(createTaskBody(external_id,content))
+        })
+      const details = await response.json()
+      console.log("CREATE TASK RESPONSE:", details)
+      return details
+    } catch (e) {
+      console.log("error from server in createTask:", e)
+      return `Error:${e}`
+    }
+  }
+
+  const createTaskBody = (external_id,content) => {
+    return (
+      {
+        "taskTypeId": TASK_TYPE_ID, //  "626a6745925841535833b638",
+        "appId": APP_ID, //"GOJt2zQA6B",
+        "communityId": COMMUNITY_ID,    //"624594390e5af47df0eed5ea",
+        "requesterId": external_id, //"528",
+        "goal": {
+          "name": "Help 4 for timeline master",
+          "description": `${content}`,
+          "keywords": [
+            "social interaction",
+            "lab"
+          ]
+        }
+      }
+    )
+  }
+
+
+  const requestToken = async (oauthCode) => {
+    try {
+      const response = await
+        fetch(`${WENET_URL}/prod/api/oauth2/token`, {
+          body: `grant_type=authorization_code&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${oauthCode}`,
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          method: "POST"
+        })
+      console.log("Response from sever:", response)
+      const content = await response.json();
+      console.log("Json content from response:", content)
+      return content;
+      /*
+      {
+        refresh_token: '8It8uoIVspPOlUUmC6Efp3L1N48Rjy9q',
+        token_type: 'bearer',
+        access_token: 'JBAsvsz1gBa9opce0jOcbN2kvxQBS45v',
+        expires_in: 7200
+      }
+      */
+    } catch (e) {
+      console.log("error from server:", e)
+      return `Error:${e}`
+    }
   
   }
+  
 
   
  exports.getAllTasks = getAllTasks;
+ exports.createNewTask = createNewTask;
+ exports.requestToken = requestToken;

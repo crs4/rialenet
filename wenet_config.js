c4f14f8bb896da7e1bd31e19fb5f4a561178acd5
Stefano Leone Monni <stefano.monni@crs4.it>
2022-06-14 14:40:00 +0200

    updated app logic (taskTypeId

--- wenet_config.js
+++ wenet_config.js
@@ -7,5 +7,5 @@ module.exports = {
   CLIENT_ID : "xUi1mwCJ0X",
   WENET_URL : "http://internetofus.u-hopper.com",
   COMMUNITY_ID : "624703620e5af47df0eed5eb",
-  TASK_TYPE_ID : "626a6745925841535833b638"
+  TASK_TYPE_ID : "62a87b53925841535833b69d"//"626a6745925841535833b638"
 };

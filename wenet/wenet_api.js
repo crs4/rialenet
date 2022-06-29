7590bf2be7953f0137d05e6c273891ebb9aa2dfa
Stefano Leone Monni <stefano.monni@crs4.it>
2022-06-29 18:51:27 +0200

    added logs and header

--- wenet/wenet_api.js
+++ wenet/wenet_api.js
@@ -94,7 +94,7 @@ const getTasks = async (tokens, goalName, requesterId) => {
       let formData = new FormData();
       formData.append("username_or_email" , username_or_email);
       formData.append("password" , password);
-      formData.append("rememberMe" , ["0","1"]);
+      formData.append("rememberMe" , "1");
 
       console.log("Login FormData:", formData);
       const response = await
